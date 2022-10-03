d65e6773a923825660d4a3da259a328473a66273
honnnoaida <honnnoaida@gmail.com>
2022-10-03 20:08:41 +0900

    対象フォルダ内でgo run main.goで動きます。

--- server/cmd/intime/main.go
+++ server/cmd/intime/main.go
@@ -1,5 +1,15 @@
-package intime
+package main
+
+import (
+	"fmt"
+
+	"github.com/gBenkyous/inTime/tree/master/server/pkg/keisan"
+)
+
+//上記に関してはgithubに上げてみんなに使ってほしいわけではないのでリモート用にする必要性はないのかもしれない。
 
 func main() {
-
+	var i int = 1
+	i = keisan.Tasizan(1, 2)
+	fmt.Println(i)
 }
